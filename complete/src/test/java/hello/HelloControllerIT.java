6a5f795e4c5839c239682e0175c58969f36e98dc
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2017-10-12 14:43:59 +0200

    Fix tests

--- complete/src/test/java/hello/HelloControllerIT.java
+++ complete/src/test/java/hello/HelloControllerIT.java
@@ -29,13 +29,13 @@ public class HelloControllerIT {
 
     @Before
     public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/");
+        this.base = new URL("http://localhost:" + port + "/app");
     }
 
     @Test
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+        assertThat(response.getBody(), equalTo("Spring Boot Started!"));
     }
 }
